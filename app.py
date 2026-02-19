1add6eeaf8cb7701339740b9f35a59e172a75efa
JSnell <113995138+JSdotSERC@users.noreply.github.com>
2026-02-19 15:24:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return "Hello everyone! My name is PyFile, and I am speaking to you from a working Azure Web App!"
-    
+    return 'Hello, Harry!'
