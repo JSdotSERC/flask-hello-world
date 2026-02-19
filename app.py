191f3ee64365924326e6babe41f282342917424d
JSnell <113995138+JSdotSERC@users.noreply.github.com>
2026-02-19 15:04:33 +0000

    Update app.py
    update to code

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
     name = input("What is your name?")
-    greeting = "Hello [name]!
-    return greeting
+    print ("Hello [name]!")
+    
